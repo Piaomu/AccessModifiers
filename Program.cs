5d0a0aea30b5578d00359a0abce2c7419bcee868
piaomu <driftwood.wahl@gmail.com>
2021-09-06 13:13:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Person
+    {
+        private string FullName;
+
+        private DateTime Birthdate;
+
+        public void SetName(string name)
+        {
+            FullName = name;
+        }
+
+        public string TellYouMyName()
+        {
+            return FullName;
+        }
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public DateTime ItsMyBirthday()
+        {
+            return Birthdate;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            // These two methods are like printing your birth certificate
+            // I'm storing data that I don't want the outside world to have.
+            person.SetName("Fred Flintstone");
+            person.SetBirthdate(new DateTime(1990, 09, 22));
+
+            // These two methods are like telling someone your name and birthday.
+            // I'm accessing the information on that certificate and giving it to the world.
+            Console.WriteLine(person.TellYouMyName());
+            Console.WriteLine(person.ItsMyBirthday());
+        }
+    }
+}
